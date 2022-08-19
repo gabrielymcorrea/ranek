c087c8c8d361bdf4cf50da7fb9400460d69fa139
gabriely <gabriely_maciel@outlook.com>
2022-08-19 13:06:01 -0300

    primeiro commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,69 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProdutoView from '../views/ProdutoView.vue'
+import LoginView from '../views/LoginView.vue'
+import UsuarioView from '../views/usuario/UsuarioView.vue'
+import UsuarioProdutos from '../views/usuario/ProdutosView.vue'
+import UsuarioVendas from '../views/usuario/VendasView.vue'
+import UsuarioEditar from '../views/usuario/EditarView.vue'
+import UsuarioCompras from '../views/usuario/ComprasView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'HomeView',
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/produto/:id',
+    name: 'ProdutoView',
+    component: ProdutoView,
+    props: true
+  },
+  {
+    path: '/login',
+    name: 'LoginView',
+    component: LoginView
+  },
+  {
+    path: "/usuario",
+    component: UsuarioView,
+    children: [
+      {
+        path: "",
+        name: "usuario",
+        component: UsuarioProdutos
+      },
+      {
+        path: "compras",
+        name: "compras",
+        component: UsuarioCompras
+      },
+      {
+        path: "vendas",
+        name: "vendas",
+        component: UsuarioVendas
+      },
+      {
+        path: "editar",
+        name: "usuario-editar",
+        component: UsuarioEditar
+      }
+    ]
+  },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
+  scrollBehavior() {
+    return window.scrollTo({ top: 0, behavior: "smooth" });
+  }
 })
+
 
 export default router
